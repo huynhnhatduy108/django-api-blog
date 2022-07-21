b5ac87498358df71c01c69e1da2c0c704c894d1b
SCB\duyhn1 <duyhn1@scb.com.vn>
2022-07-21 07:52:54 +0700

    update

---
+++ api/v1/contact/schemas.py
@@ -0,0 +1,2 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiExample, OpenApiParameter
---
+++ models/Contact/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    id = models.BigAutoField(db_column="id",primary_key=True) 
+    email = models.CharField(db_column='email', blank=True, null=True, max_length=75)
+    full_name = models.CharField(db_column='full_name', blank=True, null=True, max_length=100)
+    subject = models.CharField(db_column='subject', blank=True, null=True, max_length=100)
+    messgae = models.CharField(db_column='messgae', blank=True, null=True, max_length=500)
+    
+    created_at = models.DateTimeField(db_column='created_at', auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(db_column='updated_at', auto_now=True, blank=True, null=True)
+    hide_flag = models.IntegerField(db_column='hide_flag', blank=True, null=True, default=0)
+    deleted_flag = models.IntegerField(db_column='deleted_flag', blank=True, null=True, default=0)
+
+    def __str__(self):
+        return self.name
