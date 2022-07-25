868d29f2f788d9db83d9ac94bdb25e787a139d55
Huynh Nhat Duy <huynhnhatduy2112@gmail.com>
2022-07-25 20:46:49 +0700

    fic

--- src/settings.py
+++ src/settings.py
@@ -239,3 +239,4 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
